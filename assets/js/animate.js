6751f8bebe4259f46ca93d8c83375fa49daf33a1
Marcos Minond <minond.marcos@gmail.com>
2014-10-17 21:16:12 -0600

    animating antenna and ears

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,13 +1,42 @@
 'use strict';
 
-function wave() {
-    console.log('waving...');
+var TRANSITION_EVENTS = 'transitionend ' +
+    'webkitTransitionEnd oTransitionEnd ' +
+    'otransitionend MSTransitionEnd';
+
+function handleWave() {
+    wiggleEars(90, 50, 206, .5, function () {
+        wiggleEars(100, 40, 216, .5);
+    });
+
+    moveAntenna(1, .5, function () {
+        moveAntenna(16, .5)
+    });
 }
 
-$(function () {
-    $('body')
-        .bind('webcamSwipeLeft', wave)
-        .bind('webcamSwipeRight', wave);
-});
+function moveAntenna(deg, time, cb) {
+    $('#reddit-alien .antenna').css({
+        transition: time + 's',
+        transform: 'rotate(' + deg + 'deg)'
+    }).one(TRANSITION_EVENTS, cb || function () {});
+}
 
-$(initializeWebcamSwiper);
+function wiggleEars(top, left, right, time, cb) {
+    $('#reddit-alien .ear.left').css({
+        transition: time + 's',
+        top: top,
+        left: left
+    });
+
+    $('#reddit-alien .ear.right').css({
+        transition: time + 's',
+        top: top,
+        left: right
+    }).one(TRANSITION_EVENTS, cb || function () {});
+}
+
+initializeWebcamSwiper();
+
+$('body')
+    .bind('webcamSwipeLeft', handleWave)
+    .bind('webcamSwipeRight', handleWave);
